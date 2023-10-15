8b25de62191800b82732278c3b41e616183039f2
Fernando Lopez <itsfercodes@outlook.com>
2023-10-15 16:13:48 -0600

    Add private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import PrivateRoute from './PrivateRoute';
 
 function AppRouter() {
   return (
     <Routes>
       <Route path="login" element={<LoginPage />} />
-      <Route path="/*" element={<HeroesRoutes />} />
+      <Route
+        path="/*"
+        element={
+          <PrivateRoute>
+            <HeroesRoutes />
+          </PrivateRoute>
+        }
+      />
     </Routes>
   );
 }
